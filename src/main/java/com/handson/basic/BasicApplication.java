c44ea65142a5bef8ee6766beac8e3b5420dc0d78
root <root@ip-172-31-7-245.ap-south-1.compute.internal>
2025-05-05 13:51:55 +0300

    test commit1

--- src/main/java/com/handson/basic/BasicApplication.java
+++ src/main/java/com/handson/basic/BasicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(BasicApplication.class, args);
 	}
