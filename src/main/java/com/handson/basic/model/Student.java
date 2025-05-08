473b8090b1543f2c5b2569461ef187d1970274ef
root <shalevsaban1@gmail.com>
2025-05-08 11:38:57 +0300

    with CRUD + REST

--- src/main/java/com/handson/basic/model/Student.java
+++ src/main/java/com/handson/basic/model/Student.java
@@ -1,7 +1,10 @@
 package com.handson.basic.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.handson.basic.util.Dates;
 import org.hibernate.validator.constraints.Length;
+import org.joda.time.LocalDateTime;
 
 import javax.persistence.*;
 import javax.validation.constraints.Max;
@@ -10,6 +13,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
+
 
 @Entity
 @Table(name="student")
@@ -22,12 +26,24 @@ public class Student implements Serializable {
     @NotNull
     @Column(nullable = false, updatable = false)
     private Date createdAt = Dates.nowUTC();
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonProperty("createdAt")
+    public LocalDateTime calcCreatedAt() {
+        return Dates.atLocalTime(createdAt);
+    }
 
     @NotEmpty
     @Length(max = 60)
     private String fullname;
 
     private Date birthDate;
+
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonProperty("birthDate")
+    public LocalDateTime calcBirthDate() {
+        return Dates.atLocalTime(birthDate);
+    }
 
     @Min(100)
     @Max(800)
